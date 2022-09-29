4a1f4bbaa004c6a5ba44f50701d61b0fffe3a5b9
brewerj3 <joshuabrewer784@gmail.com>
2022-09-29 12:35:22 -1000

    Initial commit

---
+++ BallisticCalc.c
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  Ballistics03 - EE 205 - Spr 2022
+///
+/// @file BallisticCalc.c
+/// @version 1.0
+///
+/// @author Joshua Brewer <brewerj3@hawaii.edu>
+/// @date   29_Sep_2022
+///////////////////////////////////////////////////////////////////////////////
+#include "BallisticCalc.h"
+
+const double gravitationalAcceleration = 9.8;
+const double passesPerSecond = 1000;
+
+double calculateRangeMetric(double shellDiameterInMeters, double angleOfGun, double muzzleVelocityMeterPerSecond, double massOfShellKilograms){
+
+}
---
+++ BallisticCalc.h
@@ -0,0 +1,5 @@
+
+
+
+
+extern double calculateRangeMetric(double shellDiameterInMeters, double angleOfGun, double muzzleVelocityMeterPerSecond, double massOfShellKilograms);
