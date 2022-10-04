5b6cbef9db04ee6aea4cb4db46579a21f093a477
brewerj3 <joshuabrewer784@gmail.com>
2022-10-04 13:24:24 -1000

    should work just an undefined reference to fix

--- BallisticCalc.h
+++ BallisticCalc.h
@@ -1,5 +1,6 @@
 
-
 #pragma once
+
+#include <math.h>
 
 extern double calculateRangeMetric(double shellDiameterInMeters, double angleOfGunDegree, double muzzleVelocityMeterPerSecond, double massOfShellKilograms);
