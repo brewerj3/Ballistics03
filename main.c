78783abc049818631c0952c4a7f976f2e0de3538
brewerj3 <brewerj3@hawaii.edu.com>
2022-10-05 10:59:42 -1000

    Works fairly well

--- main.c
+++ main.c
@@ -4,12 +4,20 @@
 #include "BallisticCalc.h"
 
 int main() {
-    //printf("Range in Meters    = %5.2f \n",calculateRangeMetric((16 * 0.0254), 40, 762, 1225 ));
-    printf("Angle   |  Range\n");
-    printf("Degrees |    Meters     |   Yards\n");
+    printf("16""/50 Mark 7\n ");
+    printf("Angle   |   Range\n");
+    printf("Degrees |   Meters      |   Yards\n");
     double temp = 0;
     for(int i = 10; i<=45;i=i+5) {
         temp = calculateRangeMetric((16 * 0.0254), i, 762, 1225 );
+        printf("   %i   |   %5.2f    |   %5.2f \n",i, temp,(temp*1.09361));
+    }
+    printf("\nAlaska Class 12 inch Gun \n");
+    printf("Angle   |   Range\n");
+    printf("Degrees |   Meters      |   Yards\n");
+    temp = 0;
+    for(int i = 10; i<=45;i=i+5) {
+        temp = calculateRangeMetric((12 * 0.0254), i, 762, 517.093);
         printf("   %i   |   %5.2f    |   %5.2f \n",i, temp,(temp*1.09361));
     }
     return 0;
